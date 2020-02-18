1ad0d05bf7dedefe6d93f0743f210b7f3fdfd97d
ilich <st.rowdy@gmail.com>
2020-02-18 22:44:12 +0300

    TableOfTrue

---
+++ public/lesson2.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Задания:</h1>
+    <p>1. Напишите программу, которая составит и выведет в браузер таблицу истинности для логических операторов &&, || и xor.</p>
+    <p>2. Составьте функцию вычисления дискриминанта квадратного уравнения. Покройте ее тестами. Используя эту функцию, напишите программу, которая решает квадратное уравнение. Оформите красивый вывод решения.</p>
+    <nav>
+        <a href="tableOfTrue.php">Таблица истинности</a>
+        <a href="Discriminant.php">Дискриминант</a>
+    </nav>
+
+</body>
+</html>
+<?php
